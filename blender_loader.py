3a08da8f6093474905124f528cbd00c082435b1b
BananaFeet <himi86@gmail.com>
2020-11-20 18:32:29 -0800

    Added method to get file location

--- blender_loader.py
+++ blender_loader.py
@@ -1,5 +1,14 @@
+import os
+
 '''Loads maya files and exports out maya files
 to be used in Blender or another 3d software'''
+
+def get_blend_file_location(user_name):
+    '''gets file path of object to import'''
+    first = "C:/Users/"
+    last = "/Documents"
+    result = first + user_name + last
+    return result
 
 def blender_load():
     '''Loads obj files into Maya'''
--- maya_loader.py
+++ maya_loader.py
@@ -1,10 +1,22 @@
-
+import os
+#import maya.cmds as cmds
 '''Loads blender files and exports out maya files
 to be used in Blender or another 3d software'''
 
+def get_maya_file_location(user_name):
+    '''gets file path of object to import'''
+    first = "C:/Users/"
+    last = "/Documents"
+    result = first + user_name + last
+    #print(result)
+    return result
+
 def maya_load():
     '''Loads obj files into Maya'''
+    #main_path = " "
     pass
+
+
 
 def maya_export():
     '''Saves and exports as an obj.'''
