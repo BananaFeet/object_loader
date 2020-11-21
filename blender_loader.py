7b704d764030d013f1ab2f1dd6b1a99497665278
BananaFeet <himi86@gmail.com>
2020-11-20 17:36:53 -0800

    tool to import and export between maya and blender

---
+++ blender_loader.py
@@ -0,0 +1,12 @@
+'''Loads maya files and exports out maya files
+to be used in Blender or another 3d software'''
+
+def blender_load():
+    '''Loads obj files into Maya'''
+    pass
+
+def blender_export():
+    '''Saves and exports as an obj.'''
+    pass
+
+
---
+++ maya_loader.py
@@ -0,0 +1,13 @@
+
+'''Loads blender files and exports out maya files
+to be used in Blender or another 3d software'''
+
+def maya_load():
+    '''Loads obj files into Maya'''
+    pass
+
+def maya_export():
+    '''Saves and exports as an obj.'''
+    pass
+
+
